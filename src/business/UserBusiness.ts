6b0c861d591eb1e53df0fc24a1e059a956fc64bc
carolinemartins <caroline.martins.soares09@gmail.com>
2023-01-26 10:37:24 -0300

    inicialização users business

---
+++ src/business/UserBusiness.ts
@@ -0,0 +1,32 @@
+import { UserDatabase } from "../data/UserDatabase";
+import { CustomError } from "../error/CustomError"
+import { UserCreateDTO, UserInputDTO } from "../model/UserDTO";
+import { generateId } from "../services/idGenerator";
+
+export class UserBusiness {
+    userDatabase = new UserDatabase()
+
+    createUser = async ({ name, email, password }: UserInputDTO) => {
+        try {
+            if (!name || !email || !password) {
+                throw new CustomError(400, "Body invalid! name or email or password.");
+            }
+            if (password.length <= 6) {
+                throw new CustomError(400, "Password must be at least 7 characters");
+            }
+            const id = generateId()
+
+            const insertUser: UserCreateDTO = {
+                id: id,
+                name: name,
+                email: email,
+                password: password
+            }
+
+            await this.userDatabase.create(insertUser)
+
+        } catch (error: any) {
+            throw new CustomError(error.statusCode, error.message)
+        }
+    }
+}
