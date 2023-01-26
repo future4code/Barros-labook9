5aea761fe146e25e2c60f7549a12a884f54b32ea
carolinemartins <caroline.martins.soares09@gmail.com>
2023-01-26 10:30:45 -0300

    inicialização users

---
+++ src/data/UserDatabase.ts
@@ -0,0 +1,21 @@
+import { CustomError } from "../error/CustomError";
+import { UserCreateDTO } from "../model/UserDTO";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase extends BaseDatabase {
+    private static TABLE_NAME = "labook_users"
+
+    create = async ({ id, name, email, password }: UserCreateDTO): Promise<void> => {
+        try {
+            await UserDatabase.connection.insert({
+                id: id,
+                name: name,
+                email: email,
+                password: password
+            }).into(UserDatabase.TABLE_NAME)
+
+        } catch (error: any) {
+            throw new CustomError(error.statusCode, error.message)
+        }
+    }
+}
