b34e65a9997d61689936dd4abe176f4c265999f0
carolinemartins <caroline.martins.soares09@gmail.com>
2023-02-06 10:13:27 -0300

    create comment

---
+++ src/controller/CommentController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { CommentBusiness } from "../business/CommentBusiness";
+import { InputCommentDTO } from "../model/postDTO";
+
+export class CommentController{
+    constructor(private commentBusiness: CommentBusiness) { }
+
+    createComment=async(req:Request, res:Response)=>{
+        try{
+            const {comment, userId, postId} = req.body;
+
+            const insertComment:InputCommentDTO =  {
+                comment,
+                userId,
+                postId
+            }
+            await this.commentBusiness.createComment(insertComment)
+
+            res.status(201).send("Comment successfully added!")
+
+        }catch(error:any){
+            res.status(error.statusCode || 400).send(error.message || error.sqlMessage)
+        }
+    }
+}
