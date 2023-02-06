9b387aa4c93ab5ed7472b40d6a5ad60b3ef9359c
carolinemartins <caroline.martins.soares09@gmail.com>
2023-02-06 10:52:23 -0300

    retirada de comentados e inserindo identação

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,32 @@ import { likepostRouter } from "./routes/likeRouter"
 import { postRouter } from "./routes/postRouter"
 import { userRouter } from "./routes/userRouter"
 
-
+// criar usuario
 app.use("", userRouter)
 
+// criar post
 app.use("", postRouter)
 
+// buscar post de um usuario
 app.use("/posts", postRouter)
 
+// criar amizade
 app.use("/friend", friendRouter)
 
+// deletar amizade
 app.use("/friend", friendRouter)
 
+// buscar post de amigos
 app.use("/feed", feedRouter)
 
+// buscar post por type
 app.use("", feedRouter)
 
+//curtir post
 app.use("",likepostRouter)
 
+// descurtir post
 app.use("",likepostRouter)
 
+// adicionar comentarios
 app.use("/post", commentRouter)
