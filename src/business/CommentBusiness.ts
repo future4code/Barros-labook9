3228dea866fe545d452f5fc4121433a7ceaac850
carolinemartins <caroline.martins.soares09@gmail.com>
2023-02-06 10:06:09 -0300

    create comment

---
+++ src/business/CommentBusiness.ts
@@ -0,0 +1,36 @@
+import { CustomError } from "../error/CustomError";
+import { CommentDTO, InputCommentDTO } from "../model/postDTO";
+import { generateId } from "../services/idGenerator";
+import { CommentRepository } from "./CommentRepository";
+
+export class CommentBusiness {
+    constructor(private commentDatabase: CommentRepository) { }
+
+    createComment = async ({ comment, userId, postId }: InputCommentDTO): Promise<void> => {
+        try {
+            if (!comment || !userId || !postId) {
+                throw new CustomError(400, "invalid! comment or userId or postId");
+            }
+
+            if(comment === ""){
+                throw new CustomError(400, "invalid! comment or userId or postId");
+            }
+
+            
+            const id = generateId()
+
+
+            const insertComment: CommentDTO = {
+                id: id,
+                comment: comment,
+                userId: userId,
+                postId: postId
+            }
+
+            await this.commentDatabase.create(insertComment)
+
+        } catch (error: any) {
+            throw new CustomError(error.statusCode, error.message)
+        }
+    }
+}
